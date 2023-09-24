48f1d2ebaf02e56e263e19bb4fe1e84c6bbc7902
Dwiky Prias Marliansah <dwikiprias@gmail.com>
2023-09-24 12:16:11 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Dwiky Prias Marliansah",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "contact@gdwikypm.me",
+    email: "contact@dwikypm.me",
     linkedin: "dwiky-prias-marliansah",
     github: "dwikypm",
     instagram: "dwiky_pm",
