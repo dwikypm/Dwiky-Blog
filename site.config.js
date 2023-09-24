08d9fd7c4cb41c058cb610a175d09e9c194a487c
Dwiky Prias Marliansah <dwikiprias@gmail.com>
2023-09-24 11:55:51 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,21 +5,21 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "contact@gdwikypm.me",
+    linkedin: "dwiky-prias-marliansah",
+    github: "dwikypm",
+    instagram: "dwiky_pm",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Personal Portfolio`,
+      href: "https://dwikypm.me",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Dwiky Blog",
+    description: "welcome to Dwiky Blog!",
   },
 
   // CONFIG configration (required)
@@ -47,9 +47,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "dwikypm/Dwiky-Blog",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
